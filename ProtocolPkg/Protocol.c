8e5cda2b06d9036353a17f9cf3323f7597712b80
woshishabi <3488229708@qq.com>
2022-07-23 07:46:59 +0000

    build ProtocolPkg

--- ProtocolPkg/Protocol.c
+++ ProtocolPkg/Protocol.c
@@ -1,5 +1,6 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
 
 EFI_STATUS
 EFIAPI
@@ -8,6 +9,23 @@ UefiMain(
     EFI_SYSTEM_TABLE *SystemTable
     )
 {
-    Print(L"Hello, World!\n");
-    return 0;
+    EFI_STATUS Status = EFI_SUCCESS;
+    UINTN NoHandlers = 0;
+    EFI_HANDLE *Buffer = NULL;
+    Status = gBS->LocateHandleBuffer(
+        ByProtocol,
+        &gEfiGraphicsOutputProtocolGuid,
+        NULL,
+        &NoHandlers,
+        &Buffer
+    );
+
+    Print(L"Status = %d.\n", Status);
+    if(EFI_ERROR(Status))
+    {
+        Print(L"Failed to LocateHandleBuffer,\n");
+        return Status;
+    }
+    Print(L"Hello, Protocol!\n");
+    return Status;
 }
